f5022168be2cd8f3335e929be9a81a2298998094
Lua T. Andrade <andradelua2004@gmail.com>
2026-03-04 23:21:15 -0300

    Implemented the inventory system. Functions work now!

--- src/textrpg/core.clj
+++ src/textrpg/core.clj
@@ -1,10 +1,11 @@
 (ns textrpg.core
   (:gen-class)
-  (;;:require [textrpg.description :as desc]
-   :require [textrpg.menu :as menu]
-   :require [textrpg.input :as input]))
+  ;; (:require [textrpg.description :as desc]
+  ;;  :require [textrpg.input :as input]
+  ;;  :require [textrpg.inventory :as inventory])
+  )
 
 (defn -main
   "Main function of the game."
   [& args]
-  (menu/game-menu))
+  ())
--- src/textrpg/inventory.clj
+++ src/textrpg/inventory.clj
@@ -1,30 +1,31 @@
 (ns textrpg.inventory)
 
 (def inventory
-  ;; Empty atom that will store state of the inventory.
-  (atom []))
+  ;; Empty atom list that will store state of the inventory,
+  ;; I made it so I can have a mutable inventory!
+  (atom ()))
 
 (defn error-message
   "Prints out error for inventory things."
   []
   (println "You can't do that."))
 
+(defn item-in-inventory?
+  "Function will check if an item is in the inventory, returns true if so, and nil otherwise"
+  [i]
+  (if (= (some #{i} @inventory) nil)
+    nil
+    true))
+
 (defn store-item!
   "Function will add item to the vector inventory, atomically"
   [i]
-  (swap! inventory conj i)
-  )
+  (swap! inventory conj i))
 
 (defn remove-item!
   "Function will remove an item from the vector inventory, atomically"
   [i]
-  (if (= (some #{i} @inventory) nil)
-      (error-message)
-      (swap! inventory remove #{i})))
-
-(store-item! "sword")
-(store-item! "phone")
-@inventory
-
-
+  (if (= (item-in-inventory? i) true)
+    (swap! inventory #(remove #{i} %))
+    (error-message)))
 
